d617674b3dd1420ea62abc4dd9fb161040ee6c1b
francesco-abritta <fra.abritta@gmail.com>
2022-01-31 16:21:52 +0100

    header e struttura main

---
+++ src/components/Header.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+      <div class="header">
+          <img src="../assets/img/logo-small.svg" alt="">
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Header'
+}
+</script>
+
+<style>
+    .header{
+        height: 60px;
+        background-color: #2D3A46;
+        display: flex;
+        align-items: center;
+    }
+
+    img{
+        height: 40px;
+        padding-left: 20px;
+    }
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+      <div class="main">
+          <div class="items"></div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Main'
+}
+</script>
+
+<style>
+    .main{
+        height: calc(100vh - 60px);
+        background-color: #1E2D3B;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .items{
+        height: 300px;
+        background-color: red;
+    }
+</style>
