67db0d739504694d638554fa83f5d2d183bd7005
francesco-abritta <fra.abritta@gmail.com>
2022-01-31 16:46:58 +0100

    struttura base completata

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -12,7 +12,7 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
     .header{
         height: 60px;
         background-color: #2D3A46;
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,7 +1,68 @@
 <template>
   <div>
       <div class="main">
-          <div class="items"></div>
+          <div class="items">
+              <div class="card">
+                  <div class="image"><img src="https://picsum.photos/140/140" alt=""></div>
+                  <div class="title">Titolo Titolo Titolo</div>
+                  <div class="author">Autore</div>
+                  <div class="year">anno</div>
+              </div>
+              <div class="card">
+                  <div class="image"><img src="https://picsum.photos/140/140" alt=""></div>
+                  <div class="title">Titolo Titolo Titolo</div>
+                  <div class="author">Autore</div>
+                  <div class="year">anno</div>
+              </div>
+              <div class="card">
+                  <div class="image"><img src="https://picsum.photos/140/140" alt=""></div>
+                  <div class="title">Titolo Titolo Titolo</div>
+                  <div class="author">Autore</div>
+                  <div class="year">anno</div>
+              </div>
+              <div class="card">
+                  <div class="image"><img src="https://picsum.photos/140/140" alt=""></div>
+                  <div class="title">Titolo Titolo Titolo</div>
+                  <div class="author">Autore</div>
+                  <div class="year">anno</div>
+              </div>
+              <div class="card">
+                  <div class="image"><img src="https://picsum.photos/140/140" alt=""></div>
+                  <div class="title">Titolo Titolo Titolo</div>
+                  <div class="author">Autore</div>
+                  <div class="year">anno</div>
+              </div>
+              <div class="card">
+                  <div class="image"><img src="https://picsum.photos/140/140" alt=""></div>
+                  <div class="title">Titolo Titolo Titolo</div>
+                  <div class="author">Autore</div>
+                  <div class="year">anno</div>
+              </div>
+              <div class="card">
+                  <div class="image"><img src="https://picsum.photos/140/140" alt=""></div>
+                  <div class="title">Titolo Titolo Titolo</div>
+                  <div class="author">Autore</div>
+                  <div class="year">anno</div>
+              </div>
+              <div class="card">
+                  <div class="image"><img src="https://picsum.photos/140/140" alt=""></div>
+                  <div class="title">Titolo Titolo Titolo</div>
+                  <div class="author">Autore</div>
+                  <div class="year">anno</div>
+              </div>
+              <div class="card">
+                  <div class="image"><img src="https://picsum.photos/140/140" alt=""></div>
+                  <div class="title">Titolo Titolo Titolo</div>
+                  <div class="author">Autore</div>
+                  <div class="year">anno</div>
+              </div>
+              <div class="card">
+                  <div class="image"><img src="https://picsum.photos/140/140" alt=""></div>
+                  <div class="title">Titolo Titolo Titolo</div>
+                  <div class="author">Autore</div>
+                  <div class="year">anno</div>
+              </div>
+          </div>
       </div>
   </div>
 </template>
@@ -13,16 +74,42 @@ export default {
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300;400&display=swap');
+
     .main{
-        height: calc(100vh - 60px);
+        min-height: calc(100vh - 60px);
         background-color: #1E2D3B;
         display: flex;
         justify-content: center;
         align-items: center;
+        font-family: 'Roboto' , sans-serif;
     }
 
     .items{
+        display: flex;
+        flex-wrap: wrap;
+        max-width: 1000px;
+    }
+
+    .card{
         height: 300px;
-        background-color: red;
+        width: 170px;
+        padding: 10px;
+        text-align: center;
+        margin: 0px 25px 15px 0px;
+        background-color: #2D3A46;
+    }
+
+    .title{
+        color: white;
+        text-transform: uppercase;
+        font-style: bold;
+        margin: 15px 0;
+        font-size: 20px;
+    }
+
+    .author, .year{
+        color: #7A776F;
+        font-size: 15px;
     }
 </style>
